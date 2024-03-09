d85f0946a019e7650123641a8149555e9657c6eb
BrianTsangCa <110198947+BrianTsangCa@users.noreply.github.com>
2024-03-09 14:56:07 -0800

    adding products

---
+++ src/Component/Content.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Card from './Card';
+function Content() {
+  return (
+    <div class="content">
+        <h1>Explore Precision Scoop</h1>
+        <Card/>
+    </div>
+  );
+}
+
+export default Content;
