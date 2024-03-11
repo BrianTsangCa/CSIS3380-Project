decf62b0bae010c5651275e2b5f2633ff7440c09
BrianTsangCa <tyh1004@gmail.com>
2024-03-11 15:35:50 -0700

    not found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Content from './Component/Content';
 import Header from './Component/Header';
+import NotFound from './Component/NotFound';
 function App() {
   return (
     <div>
       <Header/>
-      <Content/>
+      <Routes>
+        <Route path="/" element={<Content/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/Component/NotFound.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+function NotFound() {
+  return (
+    <div id='error'>
+    <h1 className="notFoundTitle">Oops! That page can’t be found.</h1>
+    <p className="notFoundDesc">
+            It looks like nothing was found at this location.
+    </p>
+    </div>
+
+  );
+}
+
+export default NotFound;
