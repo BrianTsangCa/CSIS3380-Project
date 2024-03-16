abfb3f8df9916f04d0fc7f5cc4658e52aa122797
Kareem Katiya <130093787+HoneyB2020@users.noreply.github.com>
2024-03-16 09:21:00 -0700

    cards spaced evenly and same size
    used consistent img size

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -6,23 +6,24 @@ function Card(props) {
   return (
 
 
-<div class="card">
-  <img src={props.img} alt="product" class="card-image"/>
-  <div class="container">
-    <h4><b>{props.name}</b></h4>
-    <p class="description">{props.description}</p>
-    <div class="footer">
-      <span class="price">${props.price}</span>
-      {Array.from({ length: props.rating }, (_, index) => (
-    <span key={index}>⭐</span>
-    ))}
-  </div>
-    <div><button className="purchase-button" onClick={()=>props.addCart(props.ProductId)}>Add</button></div>
-  </div>
-</div>
+    <div class="card">
+      <img src={props.img} alt="product" class="card-image" />
+      <div class="container">
+        <h4><b>{props.name}</b></h4>
+        <p class="description">{props.description}</p>
+        <div class="footer">
+          <span class="price">${props.price}</span>
+          {Array.from({ length: props.rating }, (_, index) => (
+            <span key={index}>⭐</span>
+          ))}
+        </div>
+        <div><button className="purchase-button" onClick={() => props.addCart(props.ProductId)}>Add</button></div>
+      </div>
+    </div>
 
-  
+
   );
 }
 
 export default Card;
+
